d66d5ebdd79d1c77d10ad01ad90d90d761508964
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-06-23 11:04:12 +0530

    adding life cycle

--- Tutorial_02/app/src/main/java/com/prasadkpd/tutorial_02/MainActivity.java
+++ Tutorial_02/app/src/main/java/com/prasadkpd/tutorial_02/MainActivity.java
@@ -1,7 +1,6 @@
 package com.prasadkpd.tutorial_02;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,56 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate");
+        Log.i(TAG,"onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart");
+        Log.i(TAG,"onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG, "onRestart");
+        Log.i(TAG,"onRestart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "onPause");
+        Log.i(TAG,"onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onResume");
+        Log.i(TAG,"onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "onStop");
+        Log.i(TAG,"onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy");
+        Log.i(TAG,"onDestroy");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG, "onRestoreInstanceState");
+        Log.i(TAG,"onRestoreInstanceState");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
-        Log.i(TAG, "onSaveInstanceState");
+        Log.i(TAG,"onSaveInstanceState");
     }
-
 }
